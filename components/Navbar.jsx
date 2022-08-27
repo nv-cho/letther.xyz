1e4d63f46edb17ee8307c2cd0795a78dc0a45b81
nvcho <ignacio_0701@hotmail.com>
2022-08-27 14:39:07 -0300

    avatar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,49 +1,72 @@
-import { useContext } from "react";
+import { useEffect, useState, useContext } from "react";
+
 import { useRouter } from "next/router";
 import Link from "next/link";
-
-import { Avatar } from "@nextui-org/react";
 
 import { Context } from "../context/context";
 
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 const NavbarComponent = () => {
+  const [firstRender, setFirstRender] = useState(true);
+  const [toggle, setToggle] = useState(false);
   const router = useRouter();
 
-  const { isConnected, address } = useContext(Context);
+  const { isConnected } = useContext(Context);
+
+  useEffect(() => {
+    setFirstRender(false);
+  }, []);
 
   return (
-    <nav className="flex flex-row mt-2 p-1 justify-evenly items-center">
-      🎶
-      <ul className="flex flex-row gap-10 ">
-        <li>
-          <Link href="/">Home</Link>
-        </li>
+    !firstRender && (
+      <nav className="flex flex-row mt-2 p-1 justify-evenly items-center">
+        🎶
+        <ul className="flex flex-row gap-10">
+          <li className="navbarButton">
+            <Link href="/">Home</Link>
+          </li>
 
-        <li>
-          <Link href="/about">Explore</Link>
-        </li>
+          <li className="navbarButton">
+            <Link href="/about">Explore</Link>
+          </li>
 
-        <li>
-          <Link href="/contact">Ask</Link>
-        </li>
+          <li className="navbarButton">
+            <Link href="/contact">Ask</Link>
+          </li>
+        </ul>
+        {isConnected != true ? (
+          <ConnectButton />
+        ) : (
+          <div>
+            <button
+              onClick={() => {
+                setToggle(!toggle);
+              }}
+              className="w-12 cursor-pointer rounded-full border-2 border-green-200"
+            >
+              <img
+                className="rounded-full"
+                src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
+              />
+            </button>
 
-        <li>
-          <Link href="/contact">Profile</Link>
-        </li>
-      </ul>
-      {isConnected != true ? (
-        <ConnectButton />
-      ) : (
-        <button className="w-12 cursor-pointer rounded-full border-2 border-green-200">
-          <img
-            className="rounded-full"
-            src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
-          />
-        </button>
-      )}
-    </nav>
+            {toggle && (
+              <div className="min-w-[100px] absolute bg-white text-[#1D1D42] rounded-lg shadow-lg overflow-hidden mt-2">
+                <ul className="flex flex-col">
+                  <li className="navbarButton border-b p-2">
+                    <Link href="/profile">Profile</Link>
+                  </li>
+                  <li className="min-w-[100px] px-3 py-1 rounded-sm text-center hover:bg-red-300/80 duration-150 border-t p-2">
+                    <Link href="/logout">Logout</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        )}
+      </nav>
+    )
   );
 };
 
